f2c22b05f1fb983bda1592fe79dfc1f60c0c999c
AndrewTimoshkin <Andrew.timoshkin@gmail.com>
2022-06-14 18:43:57 +0300

    fin

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const btnStart = document.querySelector("[data-start]");
+const btnStop = document.querySelector("[data-stop]");
+const body = document.querySelector("body");
+
+let timerId = null;
+
+btnStart.addEventListener('click', showColor)
+btnStop.addEventListener("click", stopShowColor)
+
+btnStop.setAttribute("disabled", true)
+
+function showColor() { 
+timerId = setInterval(function () {
+    body.style.backgroundColor = getRandomHexColor()
+    }, 1000);    
+    
+    if (setInterval) {
+        btnStart.setAttribute("disabled", true);
+        btnStop.removeAttribute("disabled");
+    }   
+};
+
+function stopShowColor() {
+    clearInterval(timerId);
+    if (clearInterval) {
+        btnStart.removeAttribute("disabled");
+        btnStop.setAttribute("disabled", true);
+    }
+}
